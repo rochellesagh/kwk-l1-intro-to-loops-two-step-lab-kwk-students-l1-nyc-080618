09cec8a07e4bb9610c6ac1edfca1655af7e2e93d
rochellesagh <rsrochelli@gmail.com>
2018-08-08 14:03:19 +0000

    Done.

--- dance.rb
+++ dance.rb
@@ -2,7 +2,10 @@
 #output "step to the left"/"step to the right" 5 times 
 
 counter = 0 
-while counter < 5
+loop do 
+while counter > 5
+  break 
+end 
 puts "step to left"
 puts "step to right"
 counter = counter + 1
