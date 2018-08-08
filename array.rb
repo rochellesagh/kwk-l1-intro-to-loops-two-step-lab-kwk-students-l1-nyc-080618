181e554f7cb48fa8f4384ee2bb82767bf3d420ce
rochellesagh <rsrochelli@gmail.com>
2018-08-08 15:34:19 +0000

    Done.

---
+++ array.rb
@@ -0,0 +1,11 @@
+count = 0 
+
+age = ["17", "17", "16", "14"]
+
+puts age[2]
+
+puts " "
+
+book = ["Harry Potter", "Game of Thrones", "Harry Potter", "The Fault in our Stars"]
+
+puts book[1]
---
+++ shake_it_off.rb
@@ -0,0 +1,24 @@
+# puts "Cause the player's gonna"
+# puts "play" (5 times)
+# puts "And the hater's gonna"
+# puts "hate" (5 times)
+# puts "Baby, I'm just gonna..."
+def shake_it_out
+  puts "cause the player's gonna"
+  play_count = 0 
+  while play_count < 6
+  puts "play"
+  play_count += 1 
+end
+
+puts "and the hater's gonna"
+hate_count = 0 
+while hate_count < 6
+puts "hate"
+hate_count += 1 
+end
+
+puts "Baby, I'm just gonna..."
+end 
+
+shake_it_out
